4960d380ae74c77f9da40d58f68e93ac3736e9de
trkviktor <iroboteditor@gmail.com>
2024-03-20 09:49:40 +0100

    fullfit: deleteted unnecessary stuff

--- fullfittest.R
+++ fullfittest.R
@@ -7,27 +7,6 @@ RollDice <- function(N, K, P) {
     print(sum)
 }
 
-
-Summary <- function(X, Y) {
-    sum <- 0
-
-    if (missing(Y) || Y == 0) {
-        for (i in seq_along(X)) {
-            sum <- sum + X[i]
-        }
-        return(sum)
-    }
-
-
-    for (i in seq_along(X)) {
-        sum <- sum + X[i] * Y[i]
-    }
-    return(sum)
-}
-
-
-
-dice <- c(1, 2, 3, 4, 5, 6)
 k <- c(81,91,122,107,74,123)
 p <- 1/6
 
