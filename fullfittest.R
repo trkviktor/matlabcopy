eddeeeebeefd786ff9f4b72359c78568947a7324
trkviktor <iroboteditor@gmail.com>
2024-03-20 09:55:06 +0100

    fullfit works

--- fullfittest.R
+++ fullfittest.R
@@ -2,12 +2,18 @@ RollDice <- function(N, K, P) {
     n <- length(K)
     sum <- 0
     for (i in 1:n) {
-        sum <- sum + ((K[i] - (N * P))^2) / (600 * P)
+        sum <- sum + ((K[i] - (N * P[i]))^2) / (600 * P[i])
     }
-    print(sum)
+    cat(sum, "<", qchisq(p = .90, df = n - 1), "?\n")
+    cat("\n")
+    if (sum < qchisq(p = .90, df = n - 1)) {
+        cat("Az erteket elfogadjuk!\n")
+    } else {
+        cat("Az erteket nem fogadjuk el!\n")
+    }
 }
 
-k <- c(81,91,122,107,74,123)
-p <- 1/6
+k <- c(83, 91, 122, 107, 74, 123)
+p <- c(1 / 6, 1 / 6, 1 / 6, 1 / 6, 1 / 6, 1 / 6)
 
-print(RollDice(600, k, p))
+RollDice(600, k, p)
